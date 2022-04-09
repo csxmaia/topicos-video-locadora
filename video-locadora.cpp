baf15444ca4a7f30c5c1756b7de25e753fe191f4
Cristhian Maia <36846886+csxmaia@users.noreply.github.com>
2022-04-08 21:01:39 -0300

    Add files via upload

--- video-locadora.cpp
+++ video-locadora.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
+#include "clienteFuncoes.h"
 
 using namespace std;
 
 int main()
 {
 	int run = 1;
-	char opcao = "x";
-	cout << "Bem vindo ao software de gerencia de video locadoras";
+	char opcao = 'x';
+	cout << "Bem vindo ao software de gerencia de video locadoras\n\n";
 	
 	while(run) {
-		cout << "Selecione uma opcao:";
-		cout << "0 - Sair";
-		cout << "1 - Realizar operacoes com cliente";
-		cout << "2 - Realizar operacoes com o acervo";
-		cout << "3 - Realizar operacoes de locacao";
+		cout << "Selecione uma opcao:\n";
+		cout << "0 - Sair\n";
+		cout << "1 - Realizar operacoes com cliente\n";
+		cout << "2 - Realizar operacoes com o acervo\n";
+		cout << "3 - Realizar operacoes de locacao\n";
+		cout << "\n";
 		cin >> opcao;
+		
+		int iopcao = opcao - '0';
+		
+		switch(iopcao) {
+			case 1:
+				cout << "Cliente";
+				menuCliente();
+				break;
+			case 2:
+				cout << "Acervo";
+				break;
+			case 3:
+				cout << "Locacao";
+				break;
+			default: 
+				cout << "Opção inexistente";
+		}
 	}
 	return 0;
 }
