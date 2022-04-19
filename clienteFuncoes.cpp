367b43486abc71384561155e02045ab8052cf8d7
Cristhian Maia <36846886+csxmaia@users.noreply.github.com>
2022-04-19 19:59:25 -0300

    Add files via upload

--- clienteFuncoes.cpp
+++ clienteFuncoes.cpp
@@ -1,30 +1,62 @@
 #include <iostream>
 
+using namespace std;
 
-void menuCliente (char *opcao){
-	while(opcao != '0' &&
-			opcao != '1' &&
-			opcao != '2' &&
-			opcao != '3' &&
-			opcao != '4' &&
-			opcao != '5' &&){
-			cout << "Menu de Clientes" << endl;
-			cout << "1 - Adicionar um novo cliente" << endl;
-			cout << "2 - Alterar dados de um cliente" << endl;
-			cout << "3 - Remover um cliente" << endl;
-			cout << "4 - Listar TODOS clientes" << endl;
-			cin >> *opcao;		
+
+void adicionarCliente(){
+	cout << "adicionarCliente\n\n";
 }
 
-
-void adicionarCliente{
+void alterarCliente(){
+	cout << "alterarCliente\n\n";
 }
 
-void excluirCliente{
+void excluirCliente(){
+	cout << "excluirCliente\n\n";
 }
 
-void alterarCliente{
+void visualizarClientes(){
+	cout << "visualizarClientes\n\n";
 }
 
-void visualizarClientes{
+void menuCliente (){
+	int run = 1;
+	char opcao = 'x';
+	while(run){	
+		cout << "Menu de Clientes" << endl;
+		cout << "0 - Voltar" << endl;
+		cout << "1 - Adicionar um novo cliente" << endl;
+		cout << "2 - Alterar dados de um cliente" << endl;
+		cout << "3 - Remover um cliente" << endl;
+		cout << "4 - Listar TODOS clientes" << endl;
+		cin >> opcao;
+		
+		int iopcao = opcao - '0';
+		
+		switch(iopcao) {
+			case 0:
+				cout << "voltar aqui";
+				break;
+			case 1:
+				adicionarCliente();
+				break;
+			case 2:
+				alterarCliente();
+				break;
+			case 3:
+				excluirCliente();
+				break;
+			case 4:
+				visualizarClientes();
+				break;	
+			default: 
+				cout << "Opcao inexistente";
+		}
+	}
 }
+
+int main() {
+	menuCliente();
+	return 0;
+}
+
